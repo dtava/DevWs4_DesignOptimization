be32933c4b73713974a88ba87c975b947795dc8e
user <user@localhost.localdomain>
2017-10-12 07:28:29 +0200

    To be optimized

--- OptimizedCode.cpp
+++ OptimizedCode.cpp
@@ -21,22 +21,22 @@ int main(int argc, char const *argv[]) {
   // optimize initialization of vector and matrix
 
   // initialize 1e8 vector to 1
-  std::vector<double> myvector(1e8, 1.0);
-
-
-
+  std::vector<double> myvector;
+  for (int i=0; i<1e8; i++) {
+    myvector.push_back(1);
+  }
 
 
   // initialize 1e8 matrix to 1
-  std::vector<double> tmpVector(1e4, 1.0);
-  std::vector<std::vector<double>> mymatrix(1e4, tmpVector);
-  std::vector<double>().swap(tmpVector);
+  std::vector<std::vector<double>> mymatrix;
+  for (int i=0; i<1e4; i++) {
 
-
-
-
-
-
+    std::vector<double> tmpvector;
+    for (int j=0; j<1e4; j++) {
+      tmpvector.push_back(1);
+    }
+    mymatrix.push_back(tmpvector);
+  }
   // ----- END EXERCISE 1 ----- //
 
 
